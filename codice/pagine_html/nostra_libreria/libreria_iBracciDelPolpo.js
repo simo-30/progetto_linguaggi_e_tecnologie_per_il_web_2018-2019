4a3d5ae10cfdeca598dd3beb4febb33002e847fb
Simone Trenta <trenta.1724141@studenti.uniroma1.it>
2018-11-21 10:52:42 +0100

    modifiche

--- codice/pagine_html/nostra_libreria/libreria_iBracciDelPolpo.js
+++ codice/pagine_html/nostra_libreria/libreria_iBracciDelPolpo.js
@@ -96,3 +96,77 @@ function saute() {
     }
     alert("Hai ordinato un piatto di sautè di cozze e vongole");
 }
+
+function prenotaTavoli() {
+    var o=document.getElementById("butt_prenotaTavoli");
+    if (o!=null) {
+        if (document.getElementById("tavolo1").checked) {
+            scrivi_su_localStorage_tavoli("Tavolo 1", 4);
+            alert("Hai prenotato il tavolo 1 da 4 persone");
+        }
+        if (document.getElementById("tavolo2").checked) {
+            scrivi_su_localStorage_tavoli("Tavolo 2", 4);
+            alert("Hai prenotato il tavolo 2 da 4 persone");
+        }
+        if (document.getElementById("tavolo3").checked) {
+            scrivi_su_localStorage_tavoli("Tavolo 3", 4);
+            alert("Hai prenotato il tavolo 3 da 4 persone");
+        }
+        if (document.getElementById("tavolo4").checked) {
+            scrivi_su_localStorage_tavoli("Tavolo 4", 4);
+            alert("Hai prenotato il tavolo 4 da 4 persone");
+        }
+        if (document.getElementById("tavolo5").checked) {
+            scrivi_su_localStorage_tavoli("Tavolo 5", 4);
+            alert("Hai prenotato il tavolo 5 da 4 persone");
+        }
+        if (document.getElementById("tavolo6").checked) {
+            scrivi_su_localStorage_tavoli("Tavolo 6", 4);
+            alert("Hai prenotato il tavolo 6 da 4 persone");
+        }
+        if (document.getElementById("tavolo7").checked) {
+            scrivi_su_localStorage_tavoli("Tavolo 7", 4);
+            alert("Hai prenotato il tavolo 7 da 4 persone");
+        }
+        if (document.getElementById("tavolo8").checked) {
+            scrivi_su_localStorage_tavoli("Tavolo 8", 4);
+            alert("Hai prenotato il tavolo 8 da 4 persone");
+        }
+        if (document.getElementById("tavolo9").checked) {
+            scrivi_su_localStorage_tavoli("Tavolo 9", 4);
+            alert("Hai prenotato il tavolo 9 da 4 persone");
+        }
+        if (document.getElementById("tavolo10").checked) {
+            scrivi_su_localStorage_tavoli("Tavolo 10", 4);
+            alert("Hai prenotato il tavolo 10 da 4 persone");
+        }
+        if (document.getElementById("tavolo11").checked) {
+            scrivi_su_localStorage_tavoli("Tavolo 11", 6);
+            alert("Hai prenotato il tavolo 11 da 6 persone");
+        }
+        if (document.getElementById("tavolo12").checked) {
+            scrivi_su_localStorage_tavoli("Tavolo 12", 6);
+            alert("Hai prenotato il tavolo 12 da 6 persone");
+        }
+        if (document.getElementById("tavolo13").checked) {
+            scrivi_su_localStorage_tavoli("Tavolo 13", 6);
+            alert("Hai prenotato il tavolo 13 da 6 persone");
+        }
+        if (document.getElementById("tavolo14").checked) {
+            scrivi_su_localStorage_tavoli("Tavolo 14", 6);
+            alert("Hai prenotato il tavolo 14 da 6 persone");
+        }
+        if (document.getElementById("tavolo15").checked) {
+            scrivi_su_localStorage_tavoli("Tavolo 15", 6);
+            alert("Hai prenotato il tavolo 7 da 4 persone");
+        }
+        if (document.getElementById("tavolo16").checked) {
+            scrivi_su_localStorage_tavoli("Tavolo 16", 8);
+            alert("Hai prenotato il tavolo 16 da 8 persone");
+        }
+        if (document.getElementById("tavolo17").checked) {
+            scrivi_su_localStorage_tavoli("Tavolo 17", 8);
+            alert("Hai prenotato il tavolo 17 da 8 persone");
+        }
+    }
+}
