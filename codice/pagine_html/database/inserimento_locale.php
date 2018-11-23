f57ba7081a7c08865140e9337fe12fad1e4a8ac1
Simone Trenta <trenta.1724141@studenti.uniroma1.it>
2018-11-23 15:00:29 +0100

    nulla

--- codice/pagine_html/database/inserimento_locale.php
+++ codice/pagine_html/database/inserimento_locale.php
@@ -1,3 +1,5 @@
+<!--
 qui scriveremo il codice per l'inserimento del ristorante nella basi di dati con la
 lista dei ristoranti e rimanderemo il proprietario del ristorante ad un'altra
 pagina con una form per l'inserimento dei vari piatti
+--> 
