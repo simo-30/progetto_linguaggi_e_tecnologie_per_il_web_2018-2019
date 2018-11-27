00bb47a969c3a8b869d444fd5595a707de5c0484
Simone Trenta <trenta.1724141@studenti.uniroma1.it>
2018-11-27 16:50:38 +0100

    aggiunta funzione per stampare local storage

--- codice/pagine_html/pagina_Carrello_utente/carrello.js
+++ codice/pagine_html/pagina_Carrello_utente/carrello.js
@@ -1,3 +1,14 @@
 /*
     libreria per la gestione del carrello
 */
+
+function stampaStorageSemplice(){
+    var l=localStorage.length;
+    var carr=new String("<h3>Questo è il carrello</h3>");
+    var i;
+    for (i=0; i<l; i++) {
+        carr+=JSON.stringify(localStorage.getItem(localStorage.key(i)))+"<br>";
+    }
+    document.getElementById("vistaStorage").innerHTML=carr;
+    return true;
+}
