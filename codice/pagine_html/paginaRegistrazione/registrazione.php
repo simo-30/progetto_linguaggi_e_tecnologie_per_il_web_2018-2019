919b3e99446f7c0cda95fa16dc630a59925c1908
Simone Trenta <trenta.1724141@studenti.uniroma1.it>
2018-11-28 17:27:50 +0100

    ss

--- codice/pagine_html/paginaRegistrazione/registrazione.php
+++ codice/pagine_html/paginaRegistrazione/registrazione.php
@@ -2,9 +2,30 @@
     pagina in php per la registrazione di nuovi utenti
 -->
 
-<?php
+<html>
+    <head></head>
+    <body class="text-center">
 
-$dbconn = pg_connect("host=localhost port=3000 dbname=utente user=postgres password=simone") or die("Non è stato possibile connettersi al databse" . pg_last_error)
+        <?php
 
-
-?>
+            $dbconn = pg_connect("host=localhost port=3000 dbname=utente user=postgres password=simone") or die("Non è stato possibile connettersi al databse" . pg_last_error);
+            if(!(isset($_POST['registrationButton']))){
+                header("Location: ../index.html");
+            }
+            else {
+                $email = $_POST['email'];
+                $q1="select * from utente where email= $1";
+                $result=pg_query_params($dbconn, $q1, array($email));
+                if($line=pg_fetch_array($result,null,PGSQL_ASSOC)){
+                    //header("Location: alreadyRegistered.html");
+                    echo "<h1>Sei già registrato</h1>
+                    <a href=../paginaLogin/login.html> Clicca qui per il login 
+                    </a>";
+                }
+                else {
+                    
+                }
+            }
+        ?>
+    </body>
+</html>
