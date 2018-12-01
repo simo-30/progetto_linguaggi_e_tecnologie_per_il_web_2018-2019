dd8400c9d5d1be449ad61da4d5a89b849a089ba2
Simone Trenta <trenta.1724141@studenti.uniroma1.it>
2018-12-01 13:17:38 +0100

    migliore leggibilità e scalabilità del codice

--- nostra_libreria/libreria_Armando.js
+++ nostra_libreria/libreria_Armando.js
@@ -84,54 +84,6 @@ function scrivi_su_localStorage_tavoli(nome) {
     localStorage.setItem(chiave, valore);
 }
 
-function acqua1l() {
-    var o=document.getElementById("butt_acqua1l");
-    if (o!=null) {
-        scrivi_su_localStorage("Acqua 1l", 1.50);
-    }
-    alert("Hai ordinato una bottiglia di acqua da 1 litro");
-}
-
-function acqua05l() {
-    var o=document.getElementById("butt_acqua05l");
-    if (o!=null) {
-        scrivi_su_localStorage("Acqua 0,5l", 0.50);
-    }
-    alert("Hai ordinato una bottiglia di acqua da mezzo litro");
-}
-
-function cola() {
-    var o=document.getElementById("butt_cola");
-    if (o!=null) {
-        scrivi_su_localStorage("Coca-cola 0,5l", 3);
-    }
-    alert("Hai ordinato una bottiglia di coca-cola da mezzo litro");
-}
-
-function colaLattina() {
-    var o=document.getElementById("butt_colaLattina");
-    if (o!=null) {
-        scrivi_su_localStorage("Coca-cola lattina", 2);
-    }
-    alert("Hai ordinato una lattina di coca-cola");
-}
-
-function fanta() {
-    var o=document.getElementById("butt_fanta");
-    if (o!=null) {
-        scrivi_su_localStorage("Fanta 0,5l", 2.50);
-    }
-    alert("Hai ordinato una bottiglia di fanta da mezzo litro");
-}
-
-function fantaLattina() {
-    var o=document.getElementById("butt_fantaLattina");
-    if (o!=null) {
-        scrivi_su_localStorage("Fanta lattina", 1.50);
-    }
-    alert("Hai ordinato una lattina di fanta");
-}
-
 function prenotaTavoli() {
     var o=document.getElementById("butt_prenotaTavoli");
     if (o!=null) {
