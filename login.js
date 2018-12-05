397b709b5ea6a21d6aaa1ab913d99e3d8480024d
Simone Trenta <trenta.1724141@studenti.uniroma1.it>
2018-12-05 17:02:22 +0100

    sto risolvendo il bug

--- login.js
+++ login.js
@@ -5,29 +5,13 @@
 function identificati() {
     var mail=document.log.inputEmail.value;
     var passw=document.log.inputPassword.value;
-    var n, c, user;
+    var n, c, user, e;
     var i;
     var l=localStorage.length;
     for (i=0; i<l; i++) {
         var key=localStorage.key(i);
         var o=JSON.parse(localStorage.getItem(key));
-        if (o.email!="" && o.email==mail) {
-            if (o.password==passw) {
-                n=o.nome;
-                c=o.cognome;
-                user=o.username;
-                alert("Bentornato "+n+" "+c+"\nDetto "+user);
-                return true;
-            }
-            else {
-                alert("Hai inserito una password sbagliata!\nRiprova");
-                return false;
-            }
-        }
-        else {
-           alert("Riempi tutti i campi");
-           return false; 
-        }
+        
     }
     alert("Non sei ancora registrato, effettua una nuova registrazione!");
     return false;
--- paginaRegistrazione/registrazione.js
+++ paginaRegistrazione/registrazione.js
@@ -1,7 +1,6 @@
 /*
     libreria per la gestione di ogni nuova registrazione
 */
-var num_utente=0;
 
 function controllaSeGiaPresente(e) {
     var i;
@@ -18,10 +17,6 @@ function controllaSeGiaPresente(e) {
 function registra() {
     var n, c, e, p, u;
     e=document.reg.inputEmail.value;
-    if (e=="") {
-        alert("Riempi tutti i campi!!");
-        return false;
-    }
     if (controllaSeGiaPresente(e)) {
         alert("Sei già registrato, accedi con le tue credenziali");
         return false;
@@ -30,15 +25,10 @@ function registra() {
     c=document.reg.inputCognome.value;
     p=document.reg.inputPassword.value;
     u=document.reg.inputUsername.value;
-    if (n=="" || c=="" || p=="" || u=="") {
-        alert("Riempi tutti i campi!!");
-        return false;
-    }
     var ut={nome: n, cognome: c, email: e, password: p, username: u};
     var valore=JSON.stringify(ut);
     var chiave="Utente_"+e;
     localStorage.setItem(chiave, valore);
-    num_utente+=1;
     alert("Ti sei registrato con successo\nAccedi con le credenziali appena inserite");
     return true;
 }
