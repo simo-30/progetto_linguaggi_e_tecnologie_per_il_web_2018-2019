e42652dc608c55b0e85a60cbdeb8d38f6efafa42
Simone Trenta <trenta.1724141@studenti.uniroma1.it>
2018-12-13 15:53:31 +0100

    check form pagamento is ok

--- pagina_Pagamento_Ordine/pagamento.js
+++ pagina_Pagamento_Ordine/pagamento.js
@@ -29,5 +29,37 @@ function checkForm() {
     if (checkStringIsEmpty(m)) {
         return false;
     }
+    if (isNaN(m)) {
+        alert("Inserisci un numero per il mese di scadenza");
+        return false;
+    }
+    if (m<1 || m>12) {
+        alert("Il mese di scadenza deve essere compreso tra 1 e 12 inclusi");
+        return false;
+    }
+    var a=document.getElementById("anno").value;
+    if (checkStringIsEmpty(a)) {
+        return false;
+    }
+    if (isNaN(a)) {
+        alert("L'anno di scadenza deve essere un numero");
+        return false;
+    }
+    if (a<2019 || a>2099) {
+        alert("L'anno di scadenza deve essere un numero maggiore di 2018 e minore di 2099");
+        return false;
+    }
+    var c=document.getElementById("cvv").value;
+    if (checkStringIsEmpty(c)) {
+        return false;
+    }
+    if (isNaN(c)) {
+        alert("Il CVV deve essere un numero");
+        return false;
+    }
+    if (c<100 || c>999) {
+        alert("Il CVV deve essere un numero di 3 cifre");
+        return false;
+    }
     return true;
 }
