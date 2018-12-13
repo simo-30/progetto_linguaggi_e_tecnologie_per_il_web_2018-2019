b37c1e0876119062d8770e5018cd9852ac89084b
Simone Trenta <trenta.1724141@studenti.uniroma1.it>
2018-12-13 15:37:37 +0100

    cosine

--- pagina_Pagamento_Ordine/pagamento.js
+++ pagina_Pagamento_Ordine/pagamento.js
@@ -5,9 +5,29 @@
 function checkStringIsEmpty(str) {
     if (str=="") {
         alert("Riempi tutti i campi!");
-        return false;
-    }
-    else {
         return true;
     }
+    else {
+        return false;
+    }
+}
+
+function checkForm() {
+    var nome=document.getElementById("nomeCarta").value;
+    if (checkStringIsEmpty(nome)) {
+        return false;
+    }
+    var num=document.getElementById("numeroCarta").value;
+    if (checkStringIsEmpty(num)) {
+        return false;
+    }
+    if (isNaN(num)) {
+        alert("Inserisci un numero della carta corretto");
+        return false;
+    }
+    var m=document.getElementById("mese").value;
+    if (checkStringIsEmpty(m)) {
+        return false;
+    }
+    return true;
 }
